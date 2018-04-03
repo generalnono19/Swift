8976d4e452165a5f5f0ea1d785242fb8effe2606
Nourallah <nourallahtq@yahoo.com>
2018-04-03 10:51:30 +0200

    Editing
    Edite Swift examples

---
+++ list of strings.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var words = ["Hello", "World", "in", "a", "frame"]
+
+print("*********")
+for i in 0...4 {
+    print("*",words[i],"*")
+}
+print("*********")
+
+
--- odd positions in a list.playground/Contents.swift
+++ odd positions in a list.playground/Contents.swift
@@ -2,13 +2,10 @@
 import UIKit
 
 var elements = [3,6,9,11,15,76,79]
-
-func oddNumber (numbers: Array<Any>) {
     
     for odd: Int in elements {
         if odd % 3 == 0 {
             print("\(odd) is odd number")
         }
     }
-}
-oddNumber(numbers: elements as Array)
+
