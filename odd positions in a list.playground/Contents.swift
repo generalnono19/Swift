97e65bd48bbb3565e5dd3dd95ed9ee2bf705b071
Nourallah <nourallahtq@yahoo.com>
2018-04-05 11:18:24 +0200

    Editing odd example
    Editing

--- odd positions in a list.playground/Contents.swift
+++ odd positions in a list.playground/Contents.swift
@@ -1,13 +1,9 @@
 
 import UIKit
 
-var elements = [3,6,9,11,15,76,79]
-
-var odd = 0
-
-    for odd in elements {
-        if odd % 3 == 0 {
-            print("\(odd) is odd number")
-        }
-    }
-
+var array = [43,34,3,12,65,23]
+var i = 1
+while i < array.count {
+    print("this number position \(array[i]) is odd number ")
+    i += 2
+}
